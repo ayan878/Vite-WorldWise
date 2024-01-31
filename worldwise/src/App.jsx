b0c95268c1fcbf8ab821a5caa9b6956a9a9d086a
ayan878 <ayanraza197@gmail.com>
2024-01-31 23:55:46 +0530

    worldWise

--- worldwise/src/App.jsx
+++ worldwise/src/App.jsx
@@ -1,9 +1,22 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Product from "./pages/Product";
+import Pricing from "./pages/Pricing";
+import Home from "./pages/Home";
 
 function App() {
+  return (
+    <div>
+      <h1>Hello Routes!</h1>
 
-  return <BrowserRouter><Routes><Route path="product" element={<Product/>}/></Routes></BrowserRouter>;
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="product" element={<Product />} />
+          <Route path="pricing" element={<Pricing />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
