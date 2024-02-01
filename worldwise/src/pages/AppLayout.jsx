23fe40e6b4cf7ee38faa63e0920483831c173c58
ayan878 <ayanraza197@gmail.com>
2024-02-01 23:24:31 +0530

    worldWise

--- worldwise/src/pages/AppLayout.jsx
+++ worldwise/src/pages/AppLayout.jsx
@@ -1,9 +1,11 @@
 import styles from './AppLayout.module.css'
 import SideBar from "../components/SideBar";
 import Map from '../components/Map';
+import { Outlet } from 'react-router-dom';
 function AppLayout() {
   return <div className={styles.app}>
     <SideBar/>
+    <Outlet/>
     <Map/>
   </div>;
 }
