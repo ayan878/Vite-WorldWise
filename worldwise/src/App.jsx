d117170a7646376d99f5ecba0c2db8b756f337e6
ayan878 <ayanraza197@gmail.com>
2024-02-01 15:15:23 +0530

    worldWise

--- worldwise/src/App.jsx
+++ worldwise/src/App.jsx
@@ -1,9 +1,11 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
-import Home from "./pages/Home";
+import Homepage from "./pages/Homepage";
 import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./pages/AppLayout";
+import Login from "./pages/Login";
+
 
 function App() {
   return (
@@ -11,9 +13,10 @@ function App() {
       {/* <h1>Hello React!</h1> */}
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Homepage />} />
           <Route path="product" element={<Product />} />
           <Route path="pricing" element={<Pricing />} />
+          <Route path="login" element={<Login />} />
           <Route path="app" element={<AppLayout />} />
           <Route path="*" element={<PageNotFound />} />
         </Routes>
