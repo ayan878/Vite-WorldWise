426ebcaf3ba13ff208fa3ff7858113dca053f4e5
ayan878 <ayanraza197@gmail.com>
2024-02-01 17:20:35 +0530

    worldWise

--- worldwise/src/pages/AppLayout.jsx
+++ worldwise/src/pages/AppLayout.jsx
@@ -1,7 +1,11 @@
-import AppNav from "../components/AppNav";
-
+import styles from './AppLayout.module.css'
+import SideBar from "../components/SideBar";
+import Map from '../components/Map';
 function AppLayout() {
-  return <div><AppNav/></div>;
+  return <div className={styles.app}>
+    <SideBar/>
+    <Map/>
+  </div>;
 }
 
 export default AppLayout;
